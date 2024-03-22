220e0ac880d5f295ab0e906f8a7d9615d510c3f2
Русин Денис <denis.rusin2018@yandex.ru>
2024-03-22 21:40:09 +0300

    Проект 9 Версия 1.8

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-import manager.Managers;
-import manager.TaskManager;
-
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Поехали!");
-
-        TaskManager manager = Managers.getDefault();
     }
 }
